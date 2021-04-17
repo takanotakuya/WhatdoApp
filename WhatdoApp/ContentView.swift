f8575cb2e0ee2fb4f04c322abfc87c37637079a3
takanotakuya <tmw9898@icloud.com>
2021-04-17 15:38:03 +0900

    構造体の別ファイル作成

--- WhatdoApp/ContentView.swift
+++ WhatdoApp/ContentView.swift
@@ -7,17 +7,6 @@
 
 import SwiftUI
 
-struct Task: Identifiable {
-    let id = UUID()
-    var title: String
-    var checked: Bool
-    
-    init(title: String, checked: Bool) {
-        self.title = title
-        self.checked = checked
-    }
-}
-
 struct ContentView: View {
     var userData = UserData()
     
