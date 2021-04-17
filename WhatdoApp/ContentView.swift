d323069455357050d2e0a6342bafd21eee783a08
takanotakuya <tmw9898@icloud.com>
2021-04-17 15:35:28 +0900

    タスク内容の別ファイル作成

--- WhatdoApp/ContentView.swift
+++ WhatdoApp/ContentView.swift
@@ -19,16 +19,12 @@ struct Task: Identifiable {
 }
 
 struct ContentView: View {
-    var tasks = [
-        Task(title: "散歩", checked: true),
-        Task(title: "料理", checked: false),
-        Task(title: "筋トレ", checked: true)
-    ]
+    var userData = UserData()
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(tasks) { task in
+                ForEach(userData.tasks) { task in
                     ListRow(task: task.title, isCheck: task.checked)
                 }
                 
---
+++ WhatdoApp/UserData.swift
@@ -0,0 +1,16 @@
+//
+//  UserData.swift
+//  WhatdoApp
+//
+//  Created by 髙野拓弥 on 2021/04/17.
+//
+
+import SwiftUI
+
+struct UserData {
+    var tasks = [
+        Task(title: "散歩", checked: true),
+        Task(title: "料理", checked: false),
+        Task(title: "筋トレ", checked: true)
+    ]
+}
