fa115a38e628130d5a54854e53315461c716d0d9
takanotakuya <tmw9898@icloud.com>
2021-04-17 14:40:40 +0900

    Initial Commit

---
+++ WhatdoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatdoApp
+//
+//  Created by 髙野拓弥 on 2021/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatdoApp/WhatdoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatdoAppApp.swift
+//  WhatdoApp
+//
+//  Created by 髙野拓弥 on 2021/04/17.
+//
+
+import SwiftUI
+
+@main
+struct WhatdoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
